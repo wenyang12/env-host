7c584617c04adcc67ef1f512a07d891029234ce0
罗瑛 <luoying@fxiaoke.com>
2016-08-11 13:55:40 +0800

    create env-host npm module

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * 根据环境选择域名：主域名和静态资源域名
+ * @author luoying
+ */
+
+'use strict';
+
+// 站点列表
+const SITES = {
+  www: 'fxiaoke',
+  fsceshi: 'fsceshi',
+  fte: 'fsfte',
+  fte2: 'fsfte2',
+  pte: 'fspte',
+  sde: 'fssde'
+};
+
+const args = process.argv;
+// get environment: sde pte fte fte2 fsceshi www
+const envArg = args.find((arg) => arg.indexOf('--env') >= 0);
+
+const ENV = envArg && envArg.split('=')[1] || 'www';
+const SITE = SITES[ENV] || 'fxiaoke';
+const HOST = `//www.${SITE}.com`;
+const STATIC = `//${ENV}.fspage.com`;
+
+// 获取主域名
+exports.getHost = () => HOST;
+
+// 获取静态资源域名
+exports.getStaticHost = () => STATIC;
